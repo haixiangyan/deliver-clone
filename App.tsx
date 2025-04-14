f4a6d6ef369694675afdbfe84c2789c709890d23
haixiangyan <haixiang6123@gmail.com>
2025-04-14 23:19:28 +0800

    feat: 添加 tailwind 和 react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-400">Home</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
